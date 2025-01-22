682e09747db7661448d6884f07efef358a1ae5b4
Azharudeen S <azharudeen.s@bigtappanalytics.com>
2025-01-22 15:33:56 +0530

    sec code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_extras.add_vertical_space import add_vertical_space
+#from streamlit_extras.add_vertical_space import add_vertical_space
 import os
 import json
 from dotenv import load_dotenv
